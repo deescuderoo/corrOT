ed67e898c008188ad7af8dc593ebf338c38ebc15
Daniel Escudero <deescuderoo@unal.edu.co>
2018-06-14 16:32:35 +0300

    PRG works (still waiting for Assi's fix to be able to use only one PRG)

--- OT.cpp
+++ OT.cpp
@@ -75,3 +75,72 @@ void PartyOT::setChannel(){
 
     }
 };
+
+/*
+constexpr char hexmap[] = {'0', '1', '2', '3', '4', '5', '6', '7',
+                           '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
+
+std::string hexStr(byte * data, int len)
+{
+    std::string s(len * 2, ' ');
+    for (int i = 0; i < len; ++i) {
+        s[2 * i]     = hexmap[(data[i] & 0xF0) >> 4];
+        s[2 * i + 1] = hexmap[data[i] & 0x0F];
+    }
+    return s;
+}
+*/
+void printN (byte * bytes, int N) {
+    cout << hex;
+    for (int i = 0; i<N; i++ )
+        cout << (int)bytes[i];
+    cout << dec << endl;
+}
+
+void SenderOT::generateKeysOT() {
+    // Fills in the buffers keys0_bOT and keys1_bOT with (pseudo)random bytes
+//    for (uint i = 0; i < keys0_bOT.size(); i++) {
+//        byte * p0 = prg->getPRGBytesEX(sizeof(data_t));
+//        memcpy(keys0_bOT[i].bytes, p0, sizeof(data_t));
+////        cout << (long int)keys0_bOT[i].bytes << endl;
+//        cout << hexStr(p0, sizeof(data_t)) << endl;
+//
+//        byte * p1 = prg->getPRGBytesEX(sizeof(data_t));
+//        memcpy(keys1_bOT[i].bytes, p1, sizeof(data_t));
+////        cout << (long int)keys1_bOT[i].bytes << endl;
+//        cout << hexStr(p1, sizeof(data_t)) << endl;
+    size_t size = sizeof(data_t)* keys0_bOT.size();
+    cout << "size " << size << endl;
+    memcpy(keys0_bOT[0].bytes,prg0->getPRGBytesEX(size),size);
+    memcpy(keys1_bOT[0].bytes,prg1->getPRGBytesEX(size),size);
+
+//    for (int i = 0; i < 5; i++) {
+//        printN(keys0_bOT[i].bytes, 16);
+//    }
+//
+//    for (int i = 0; i < 5; i++) {
+//        printN(keys1_bOT[i].bytes, 16);
+//    }
+
+    //printN(keys1_bOT[0].bytes, 32);
+
+
+
+    /*
+    for (uint i = 0; i < keys0_bOT.size(); i++) {
+
+        memcpy (keys0_bOT[i].bytes , prg->getPRGBytesEX(sizeof(data_t)), sizeof(data_t));
+         cout << hexStr(keys0_bOT[i].bytes, sizeof(data_t)) << endl;
+
+        byte * p1 = prg->getPRGBytesEX(sizeof(data_t));
+        memcpy(keys1_bOT[i].bytes, p1, sizeof(data_t));
+
+
+*/
+
+ }
+
+void ReceiverOT::runInitialize() {
+
+}
+
--- OT.hpp
+++ OT.hpp
@@ -2,46 +2,81 @@
 #include <libscapi/include/interactive_mid_protocols/OTExtensionBristol.hpp>
 #include <libscapi/include/primitives/Prg.hpp>
 
+// Number of OTs
+#define CONST_k 128
+// Length for the correlated OTs
+#define CONST_n 1000
+// Length of masking correlated vector
+#define CONST_k_ 1
+// Length of base OTs
+#define SIZE_OT CONST_k/8
+
 void OT(int my_num, shared_ptr<CommParty> channel);
+
+struct data_t
+{
+    data_t(string s){
+        memset(bytes,0,sizeof(data_t));
+        memcpy(bytes, s.c_str(), s.length());
+    }
+    byte bytes[SIZE_OT];
+
+    data_t(){
+        memset(bytes,0,sizeof(data_t));
+    }
+};
 
 class PartyOT {
 private:
     int id;
     shared_ptr<CommParty> channel;  // Channel between this party to every other party in the protocol.
-    PrgFromOpenSSLAES* prg;
+    void setChannel();
 
 public:
     PartyOT(int id)
-            : id(id), prg(new PrgFromOpenSSLAES()){
+            : id(id), prg0(new PrgFromOpenSSLAES()), prg1(new PrgFromOpenSSLAES()) {
         // Sets the channel
         setChannel();
 
-        SecretKey secretKey = prg->generateKey(256); // Sets the key for the PRG
-        prg->setKey(secretKey);
+        SecretKey secretKey0 = prg0->generateKey(256); // Sets the key for the PRG
+        prg0->setKey(secretKey0);
+        SecretKey secretKey1 = prg1->generateKey(256); // Sets the key for the PRG
+        prg1->setKey(secretKey1);
     }
 
     int getID() { return id; }
-    void setChannel();
+
     shared_ptr<CommParty> getChannel() { return channel; }
 
-//    byte * p = prg->getPRGBytesEX(sizeof(data256));
-//    memcpy(iotape[m_partyId].bytes, p, sizeof(data256));
-};
-
-class SenderOT : public PartyOT{
-public:
-    SenderOT() : PartyOT(0) {}
-
-    void runInitialize();
-
-private:
-
+protected:
+    PrgFromOpenSSLAES * prg0;
+    PrgFromOpenSSLAES * prg1;
 };
 
 class ReceiverOT : public PartyOT{
 public:
     ReceiverOT() : PartyOT(1) {}
 
-private:
+    void runInitialize();
 
+private:
+    vector<data_t> choice_bits; // Holds the receiver's choice bits for the OTs
+
+    void generateChoiceBitsOT();
+
+};
+
+class SenderOT : public PartyOT{
+public:
+    SenderOT() : PartyOT(0), keys0_bOT(CONST_k), keys1_bOT(CONST_k) {
+        generateKeysOT();
+    }
+
+    void runInitialize();
+
+private:
+    vector<data_t> keys0_bOT; // Holds the sender's inputs to the OTs
+    vector<data_t> keys1_bOT;
+
+    void generateKeysOT();
 };
